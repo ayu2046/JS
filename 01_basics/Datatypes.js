84f98f5b541b111fff20a6e7ff856e8aa3a7f26f
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-06-15 20:33:31 +0000

    conversion of data types

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //treat all js code as a newer version
+ 
+//datatype:
+//null=> is like a object
+// string=> contains character value
+//integer=> contains numbers
+//undefined=>
+//object
+//boolean=> true/false
+//bigint 
+console.log("ayush")
+let a=18
+let b="a"
+let c=18.5
+console.log(typeof null)
---
+++ 01_basics/conversion_operator.js
@@ -0,0 +1,15 @@
+let score = 33
+ console.log(typeof score) 
+ console.log(typeof (score)) 
+
+ let isloggedin = 0
+
+ let booleanisloggedin = Boolean(isloggedin)
+
+ console.log(booleanisloggedin)
+
+ let sumnum = 33
+ let stringnum = String(sumnum)
+
+ console.log(stringnum)
+ console.log(typeof(stringnum))
