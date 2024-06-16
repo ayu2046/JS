01416b89e1e7a06e10a3f54a0f482e22f15df36a
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-06-16 19:57:43 +0000

    comparison

--- 01_basics/conversion_operator.js
+++ 01_basics/conversion_operator.js
@@ -1,15 +1,30 @@
-let score = 33
- console.log(typeof score) 
- console.log(typeof (score)) 
+// let score = 33
+//  console.log(typeof score) 
+//  console.log(typeof (score)) 
 
- let isloggedin = 0
+//  let isloggedin = 0
 
- let booleanisloggedin = Boolean(isloggedin)
+//  let booleanisloggedin = Boolean(isloggedin)
 
- console.log(booleanisloggedin)
+//  console.log(booleanisloggedin)
 
- let sumnum = 33
- let stringnum = String(sumnum)
+//  let sumnum = 33
+//  let stringnum = String(sumnum)
 
- console.log(stringnum)
- console.log(typeof(stringnum))
+//  console.log(stringnum)
+//  console.log(typeof(stringnum))
+
+ //************************operators****************************** */
+
+ let value =3
+ let negvalue = -value
+//  console.log(negvalue)
+
+//  char1 = "hello"
+//  char2 = " ayush"
+//  char3 = char1 + char2
+//  console.log(char3)
+
+//  console.log("1" + 2)
+//  console.log(1 + 2+ "3")
+//  console.log("1" + 2 + 2)
