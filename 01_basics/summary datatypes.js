98176c23eaef3dcca907886081a02de3707f80fe
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-06-19 19:51:05 +0000

    strings

--- 01_basics/summary datatypes.js
+++
@@ -1,7 +0,0 @@
-//Primitive: Number, String, Boolean, Null, Undefined, Symbol, Bigint
-
-//Reference - Non-primitive: Arrays, Objects, Functions
-
-//====================================================================================================
-
-//stack(Primitive),   Heap(non-Primitive)
