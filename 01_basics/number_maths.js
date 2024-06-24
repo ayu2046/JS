dc3b09ed7580acd1f47c5c8c70a70d24f25d8e77
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-06-24 04:32:01 +0000

    comparison

---
+++ 01_basics/number_maths.js
@@ -0,0 +1,22 @@
+// const score = 300
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const other = 20.345
+// const num = 1000000
+// console.log(other.toPrecision(2));
+// console.log(num.toLocaleString());
+// console.log(num.toLocaleString('en-IN'));
+
+// //++++++++++++++++++++++++++Maths++++++++++++++++++++++++++
+//  console.log(Math);
+//  console.log(Math.abs(-4))
+//  console.log(Math.round(4.34))
+//  console.log(Math.ceil(4.2))
+//  console.log(Math.floor(4.9))
+// console.log(Math.random()*10)
+
