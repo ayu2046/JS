a29e891d27a7b2f7f0a9e1cd86448b301b31b47c
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-06-24 04:55:01 +0000

    dates

---
+++ 01_basics/Dates.js
@@ -0,0 +1,12 @@
+// let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toLocaleTimeString())
+// console.log(mydate.toString())
+
+// let mycreateddate= new Date(2023, 0, 23)
+// console.log(mycreateddate.toDateString());
