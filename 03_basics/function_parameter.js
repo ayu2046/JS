c3a413e6b9d859cf91829a6f32169c49dd027ef9
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-06-25 20:53:10 +0000

    function

---
+++ 03_basics/function_parameter.js
@@ -0,0 +1,26 @@
+// console.log("A");
+// console.log("y");
+// console.log("u");
+// console.log("s");
+// console.log("h");
+
+// function print(){
+//     console.log("A");
+//     console.log("y");
+//     console.log("u");
+//     console.log("s");
+//     console.log("h");
+// }
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// print()
+// add(2,3)
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
