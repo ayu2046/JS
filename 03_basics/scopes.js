041327776244b3fff27966aa4aaf1b14e3849c51
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-07-01 19:57:12 +0000

    scopes

---
+++ 03_basics/scopes.js
@@ -0,0 +1,18 @@
+// let a = 100//global variable
+// if(true){
+//     let a = 10//local variable
+//     const b = 20
+//     var c = 30
+//     console.log("inner:", a);
+// }
+// console.log(a);
+function one(){
+    const name ="ayush";
+    function two(){
+        const website = 'github';
+        console.log(name);
+    }
+    two()
+    console.log(name);
+}
+one()
