6b280880f1459dca2c634631a0d02b06751613cf
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-07-10 05:52:03 +0000

    control flow

---
+++ 04_controlflow/Switch.js
@@ -0,0 +1,32 @@
+// switch (1) {
+//     case 1:
+//         console.log("hlo1");
+//         break;
+//     case -1:
+//         console.log("hlo2");
+//         break;
+//     default:
+//         console.log("hello");
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
