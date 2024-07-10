97fd4cb6f2964b1d5da131080627397aa77f504e
Ayush Awasthi <89137905+ayu2046@users.noreply.github.com>
2024-07-10 04:33:13 +0000

    initial

---
+++ 03_basics/iife.js
@@ -0,0 +1,12 @@
+// Immediaely Invoked function expression
+//named iife
+(function code(){
+    console.log("DB done");
+})();
+
+//while writing two iife seperate them with ;
+//unnamed iife
+// arrow function
+(() => {
+    console.log("DB connected two");
+}) ()
